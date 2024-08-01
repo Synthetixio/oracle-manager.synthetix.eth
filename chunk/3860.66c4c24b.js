921f36d7b1922c06251ed51f2bc4605048ec17a2
synthetix-team <engineering@snxdao.io>
2024-08-01 10:45:03 +0000

    Release
    Source: https://github.com/Synthetixio/v3ui
    
    Commit: 27f56c8bce49200bbf04a5598c111fa29c59040a

---
+++ chunk/3860.66c4c24b.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunk_snx_v3_oracle_manager=globalThis.webpackChunk_snx_v3_oracle_manager||[]).push([[3860],{3860:(a,e,i)=>{i.d(e,{defineChain:()=>n.a,extractChain:()=>r});var n=i(4607);function r({chains:a,id:e}){return a.find((a=>a.id===e))}},4607:(a,e,i)=>{function n(a){return{formatters:void 0,fees:void 0,serializers:void 0,...a}}i.d(e,{a:()=>n})}}]);
+//# sourceMappingURL=3860.66c4c24b.js.map
